c058b08a9cc619abeea7126ebf87ac67687e1d2c
PuhlmannJulia <s0584638@htw-berlin.de>
2025-04-03 08:25:32 +0200

    Springboot Initializr

---
+++ src/main/java/com/example/WhiskyApp/WhiskyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.WhiskyApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WhiskyApp/WhiskyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WhiskyApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
